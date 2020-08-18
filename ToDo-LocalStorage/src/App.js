be6ac32bac1e2444e44fb7448d173f974bbd353e
ramneek008 <singhramneek274@gmail.com>
2020-08-18 16:38:12 +0530

    update app.js

--- ToDo-LocalStorage/src/App.js
+++ ToDo-LocalStorage/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import {Container} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import Todos from './Components/Todos';
+import TodoForm from './Components/TodoForm';
+
+const App = () => {
+
+  const [todos, setTodos] = useState([]);
+
+  return(
+    <Container fluid>
+      <h1>Todo with Local Storage</h1>
+      <Todos todos={todos} markComplete={markComplete} />
+      <TodoForm addTodos={addTodos} />
+    </Container>
   );
+
 }
 
 export default App;
